4efa7a87649c8442db60b4becabb7efbe32b68be
Gary Godderidge <godderidge@byui.edu>
2024-01-09 18:25:45 -0700

    Initial Code

---
+++ testcode.py
@@ -0,0 +1,10 @@
+# Python
+import math
+# This is a test project
+
+def calculate_circle_area(radius):
+    area = math.pi * radius * radius
+    return area
+
+circle_area = calculate_circle_area(10)
+print(f'This area of the circle is: {circle_area}')
